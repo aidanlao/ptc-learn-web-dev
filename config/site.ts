1b19cba3210ead2f5dc84a5d9a4f46d3054e1bcb
Aidan <laoaidan@gmail.com>
2025-06-12 19:12:00 -0400

    Changed site title

--- config/site.ts
+++ config/site.ts
@@ -1,8 +1,8 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-  name: "Next.js + NextUI",
-  description: "Make beautiful websites regardless of your design experience.",
+  name: "Learn Web Dev",
+  description: "Learn how to build web applications from scratch.",
   navItems: [
     {
       label: "Home",
